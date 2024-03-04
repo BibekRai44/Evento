4a82132f99da12e74ed6325bef0a2d514938f526
Bibek Rai <bibekrai655@gmail.com>
2024-03-04 09:19:24 +0545

    routes for new field

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, url_for, redirect,request,flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import configure_uploads, IMAGES, UploadSet
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from flask_wtf.file import FileField, FileAllowed
 from sqlalchemy import inspect
 import os
+
 
 
 
@@ -15,6 +20,7 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
 
@@ -40,7 +46,9 @@ class Event(db.Model):
     location = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(1024), nullable=False)
     time = db.Column(db.String(100), nullable=False)
-
+    organizer = db.Column(db.String(100), nullable=False)
+    contactorganizer = db.Column(db.String(100), nullable=False)
+    
 db.create_all()
 
 inspector = inspect(db.engine)
@@ -90,53 +98,55 @@ class eventForm(FlaskForm):
 
     description = StringField(validators=[
                            InputRequired(), Length(min=4, max=1024)], render_kw={"placeholder": "Description"})
+    organizer = StringField(validators=[
+                           InputRequired(), Length(min=4, max=100)], render_kw={"placeholder": "Organizer"})
+    contactorganizer = StringField(validators=[
+                           InputRequired(), Length(min=4, max=100)], render_kw={"placeholder": "Contact Organizer"})
     time=StringField(validators=[
                            InputRequired(), Length(min=4, max=100)], render_kw={"placeholder": "Time"})
-    
+ 
 
     submit = SubmitField('Post Event')
 
     def validate_eventname(self, eventname):
         existing_eventname = Event.query.filter_by(
             eventname=eventname.data).first()
-        if existing_eventname:
-            raise ValidationError(
-                'That event name already exists. Please choose a different one.')
-
+        
     def validate_duration(self, duration):
         existing_duration = Event.query.filter_by(
             duration=duration.data).first()
-        if existing_duration:
-            raise ValidationError(
-                'That duration already exists. Please choose a different one.')
+        
     
     def validate_time(self, time):
         existing_time = Event.query.filter_by(
             time=time.data).first()
-        if existing_time:
-            raise ValidationError(
-                'That time already exists. Please choose a different one.')
+        
 
     def validate_date(self, date):
         existing_date = Event.query.filter_by(
             date=date.data).first()
-        if existing_date:
-            raise ValidationError(
-                'That date already exists. Please choose a different one.')
+        
 
     def validate_location(self, location):
         existing_location = Event.query.filter_by(
             location=location.data).first()
-        if existing_location:
-            raise ValidationError(
-                'That location already exists. Please choose a different one.')
+        
+    
+    def validate_organizer(self, organizer):
+        existing_organizer = Event.query.filter_by(
+            organizer=organizer.data).first()
+    
+    def validate_contactorganizer(self, contactorganizer):
+        existing_contactorganizer = Event.query.filter_by(
+            contactorganizer=contactorganizer.data).first()
+        
 
     def validate_description(self, description):
         existing_description = Event.query.filter_by(
             description=description.data).first()
-        if existing_description:
-            raise ValidationError(
-                'That description already exists. Please choose a different one.')
+        
+        
+    
 
 @app.route('/')
 def home():
@@ -176,20 +186,26 @@ def dashboard():
 @login_required
 def post_event():
     form = eventForm()
-    print("form data",request.form)
+    
     if form.validate_on_submit():
+        print("Form data:", request.form)
         event = Event(
             eventname=form.eventname.data,
             duration=form.duration.data,
             date=form.date.data,
             location=form.location.data,
             description=form.description.data,
-            time=form.time.data
+            time=form.time.data,
+            organizer=form.organizer.data,
+            contactorganizer=form.contactorganizer.data
+            
         )
         db.session.add(event)
         db.session.commit()
         flash('Event posted successfully!', 'success')
         return redirect(url_for('dashboard'))
+    else:
+        print("Form validation failed. Errors:", form.errors)
     return render_template('post_event.html', form=form)
 
 @ app.route('/register', methods=['GET', 'POST'])
