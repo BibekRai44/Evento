c4a6537ecc11e1adf9cdfaa606a44d49334add10
Bibek Rai <bibekrai655@gmail.com>
2024-03-15 08:42:49 +0545

    route for update password

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, url_for, redirect,request,flash,abort,
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user,current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError,EqualTo,Regexp
-from flask_bcrypt import Bcrypt
+from wtforms import StringField, PasswordField, SubmitField,validators
+from wtforms.validators import InputRequired, Length, ValidationError,EqualTo,Regexp,Email,DataRequired
+from flask_bcrypt import Bcrypt,generate_password_hash,check_password_hash
 from sqlalchemy import inspect
 import os
 from flask import Flask, flash, request, redirect, url_for
@@ -14,6 +14,15 @@ from sqlalchemy import or_
 from sqlalchemy.orm import relationship
 from functools import wraps
 from datetime import timedelta,datetime
+from flask_mail import Message
+import secrets
+from flask_mail import Mail
+from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from itsdangerous import URLSafeTimedSerializer
+
+
+
 
 
 app = Flask(__name__)
@@ -23,6 +32,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'bibekrai655@gmail.com'
+app.config['MAIL_PASSWORD'] = 'piux zeri dtsy dyxj'
+app.config['MAIL_DEFAULT_SENDER'] = 'bibekrai655@gmail.com'
+
+mail = Mail(app)
 
 
 db = SQLAlchemy(app)
@@ -54,7 +72,6 @@ class User(db.Model, UserMixin):
     saved_events = db.relationship('Event', secondary=saved_events, backref='saved_by')
     posted_events = db.relationship('Event', secondary=posted_events, backref='posted_by')
     
-
 
 
 
@@ -98,6 +115,10 @@ class RegisterForm(FlaskForm):
                              InputRequired(), EqualTo('password', message='Passwords must match')])
 
     submit = SubmitField('Register')
+
+class UpdatePasswordForm(FlaskForm):
+    email = StringField('Email', validators=[validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', validators=[validators.DataRequired(), validators.Length(min=8)])
 
 
     def validate_username(self, username):
@@ -227,9 +248,7 @@ def postevent():
 def service():
     return render_template('service.html')
 
-@app.route('/forgot_password')
-def forgot_password():
-    return render_template('forgot_password.html')
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -367,6 +386,72 @@ def delete_event(event_id):
     flash('Event deleted successfully!', 'success')
     return redirect(url_for('dashboard'))
 
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+            token = s.dumps(email, salt='forgot-password')
+            reset_link = url_for('update_password', token=token, _external=True)
+
+            # Send email with reset link
+            send_reset_email(email, reset_link)
+            flash('Password reset email sent. Please check your inbox.', 'info')
+            return redirect(url_for('login'))
+        else:
+            flash('Email address not found.', 'error')
+            return redirect(url_for('forgot_password'))
+    
+    return render_template('forgot_password.html')
+
+# Route for updating the password
+@app.route('/update_password/<token>', methods=['GET', 'POST'])
+def update_password(token):
+    if request.method == 'POST':
+        new_password = request.form.get('new-password')
+        confirm_password = request.form.get('confirm-password')
+
+       
+        s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+        try:
+            email = s.loads(token, salt='forgot-password', max_age=1800)  
+        except:
+            flash('Invalid or expired token.', 'error')
+            return redirect(url_for('forgot_password'))
+
+        
+        if new_password != confirm_password:
+            flash('New password and confirm password do not match.', 'error')
+            return redirect(url_for('update_password', token=token))
+
+        if len(new_password) < 8:
+            flash('New password must be at least 8 characters long.', 'error')
+            return redirect(url_for('update_password', token=token))
+
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            user.password = generate_password_hash(new_password)
+            db.session.commit()
+            flash('Password updated successfully.', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash('User not found.', 'error')
+            return redirect(url_for('forgot_password'))
+
+    
+    return render_template('update_password.html', token=token)
+
+
+def send_reset_email(email, reset_link):
+    
+    from flask_mail import Mail, Message
+    mail = Mail(app)
+    msg = Message('Password Reset', recipients=[email])
+    msg.body = f'Click the following link to reset your password: {reset_link}'
+    mail.send(msg)
 
 @app.route('/update_event/<int:event_id>', methods=['GET','POST'])
 @login_required
